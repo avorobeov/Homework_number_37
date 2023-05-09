ce6a64746f76faeef45389ebb09559fc55fc39e5
воробйов александр <avorobeov21@gmail.com>
2023-05-09 20:51:14 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_37/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_number_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dataStorage = new List<string>();
+            string[] array1 = new string[] { "1", "2", "4" };
+            string[] array2 = new string[] { "3", "5", "4" };
+
+            AddArrayInCollection(dataStorage, array1);
+            AddArrayInCollection(dataStorage, array2);
+
+            PrintCollection(dataStorage);
+        }
+
+        private static void AddArrayInCollection(List<string> collection, string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (collection.Contains(array[i]) == false)
+                {
+                    collection.Add(array[i]);
+                }
+            }
+        }
+
+        private static void PrintCollection(List<string> collection)
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                Console.Write($"{collection[i]} ");
+            }
+        }
+    }
+}
